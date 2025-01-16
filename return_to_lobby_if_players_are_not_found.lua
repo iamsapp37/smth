add4a079ca436e53155dd4bbbe43b95eb43e387a
iamsapp37 <Thiem117@gmail.com>
2025-01-16 11:02:06 +0700

    Update return_to_lobby_if_players_are_not_found.lua

--- return_to_lobby_if_players_are_not_found.lua
+++ return_to_lobby_if_players_are_not_found.lua
@@ -1,3 +1,23 @@
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Size = UDim2.new(0, 300, 0, 40)
+textLabel.Position = UDim2.new(0.5, -150, 0.8, -80)
+textLabel.Text = ""
+textLabel.TextScaled = true
+textLabel.BackgroundTransparency = 1
+textLabel.TextColor3 = Color3.fromRGB(0, 255, 255)
+textLabel.TextStrokeColor3 = Color3.new(0, 0, 0)
+textLabel.TextStrokeTransparency = 0
+textLabel.Parent = screenGui
+
+local function displayText (message)
+    textLabel.Text = message
+    wait(3)
+    textLabel.Text = ""
+end
+
 local scriptExecuted = false
 local players = game:GetService("Players")
 local replicatedStorage = game:GetService("ReplicatedStorage")
@@ -13,9 +33,11 @@ local function returnToLobby()
 end
 
 local function buangScript()
+    displayText("Executing BuangHub..!")
     script_key = _G.config.buangKey
     loadstring(game:HttpGet("https://raw.githubusercontent.com/buang5516/buanghub/main/PremiumBuangHub.lua"))()
     scriptExecuted = true
+    removeScript()
 end
 
 local function checkPlayer()
@@ -40,7 +62,12 @@ local function checkPlayer()
     end
 end
 
+local function removeScript()
+    script:Destroy()
+end
+
 while not scriptExecuted do
-    wait(_G.config.waitTime)
+    displayText("Checking players...")
     checkPlayer()
+    wait(_G.config.waitTime)
 end
