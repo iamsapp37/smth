62f77bb68c01901798c9e4d6aff84ba830421ae1
iamsapp37 <Thiem117@gmail.com>
2025-01-16 09:11:34 +0700

    Update return_to_lobby_if_players_are_not_found.lua

--- return_to_lobby_if_players_are_not_found.lua
+++ return_to_lobby_if_players_are_not_found.lua
@@ -1,28 +1,28 @@
-local waitTime = 10
-local buangKey = "put your script key in here"
-local playerNames = {
-    "buang",
-    "buang2",
-    "buang3"
-}
-local requiredPlayers = 2
-
 local scriptExecuted = false
-function isPlayerInLobby()
-    local player = game:GetService("Players").LocalPlayer
-    local currentPlaceId = game.PlaceId
-    local lobbyPlaceId = 8304191830
-    return currentPlaceId == lobbyPlaceId
+local players = game:GetService("Players")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local localPlayer = players.LocalPlayer
+local lobbyPlaceId = 8304191830
+
+local function isPlayerInLobby()
+    return game.PlaceId == lobbyPlaceId
 end
 
-function checkPlayer()
+local function returnToLobby()
+    replicatedStorage:WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("teleport_back_to_lobby"):InvokeServer("_lobbytemplategreen9")
+end
+
+local function buangScript()
+    script_key = _G.config.buangKey
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/buang5516/buanghub/main/PremiumBuangHub.lua"))()
+    scriptExecuted = true
+end
+
+local function checkPlayer()
     if scriptExecuted then return end
 
-    local players = game:GetService("Players")
-    local player = players.LocalPlayer
     local playerFound = 0
-
-    for _, playerName in ipairs(playerNames) do
+    for _, playerName in ipairs(_G.config.playerNames) do
         for _, p in ipairs(players:GetPlayers()) do
             if p.Name == playerName then
                 playerFound = playerFound + 1
@@ -31,7 +31,7 @@ function checkPlayer()
         end
     end
 
-    if playerFound < requiredPlayers then
+    if playerFound < _G.config.requiredPlayers then
         if not isPlayerInLobby() then
             returnToLobby()
         end
@@ -40,21 +40,7 @@ function checkPlayer()
     end
 end
 
-function returnToLobby()
-    local args = {
-        [1] = "_lobbytemplategreen9"
-    }
-    
-    game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("teleport_back_to_lobby"):InvokeServer(unpack(args))
-end
-
-function buangScript()
-    script_key = buangKey
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/buang5516/buanghub/main/PremiumBuangHub.lua"))()
-    scriptExecuted = true
-end
-
-while true do
-    wait(waitTime)
+while not scriptExecuted do
+    wait(_G.config.waitTime)
     checkPlayer()
 end
